67a9087128f8d1cf31b40572af9b5ef18b57a424
jeancedrazc-blip <jeancedrazc@gmail.com>
2026-08-19 19:13:16 -0300

    Mark main-mod integration mixin as non-remapped

--- src/main/java/mcjty/rftoolsbuilder/mixin/RFToolsBuilderMixin.java
+++ src/main/java/mcjty/rftoolsbuilder/mixin/RFToolsBuilderMixin.java
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "mcjty.rftoolsbuilder.RFToolsBuilder")
+@Mixin(targets = "mcjty.rftoolsbuilder.RFToolsBuilder", remap = false)
 public abstract class RFToolsBuilderMixin {
-    @Inject(method = "<init>", at = @At("RETURN"))
+    @Inject(method = "<init>", at = @At("RETURN"), remap = false)
     private void quantumtools$registerConstructor(IEventBus modBus, ModContainer container, CallbackInfo ci) {
         ConstructorBootstrap.init(modBus);
     }

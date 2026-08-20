1eb12a2be4b9de5ac6000387ed4db1f9e5e838a8
jeancedrazc-blip <jeancedrazc@gmail.com>
2026-08-20 07:31:24 -0300

    Track static and active Constructor energy rendering

--- src/main/java/mcjty/rftoolsbuilder/constructor/client/ConstructorRenderState.java
+++ src/main/java/mcjty/rftoolsbuilder/constructor/client/ConstructorRenderState.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.item.ItemStackRenderState;
 public final class ConstructorRenderState extends BlockEntityRenderState {
     public final BlockModelRenderState turret = new BlockModelRenderState();
     public final BlockModelRenderState barrel = new BlockModelRenderState();
+    public final BlockModelRenderState baseEnergyChannel = new BlockModelRenderState();
     public final BlockModelRenderState energyChannel = new BlockModelRenderState();
     public final BlockModelRenderState projectile = new BlockModelRenderState();
     public final ItemStackRenderState entityProjectile = new ItemStackRenderState();
@@ -26,5 +27,6 @@ public final class ConstructorRenderState extends BlockEntityRenderState {
     public boolean hasTarget;
     public boolean projectileVisible;
     public boolean projectileIsItem;
+    public boolean energyActive;
     public ConstructorStatus status = ConstructorStatus.IDLE;
 }

b28b28ad29e77a768ff6855459d7e035917616de
jeancedrazc-blip <jeancedrazc@gmail.com>
2026-08-21 02:41:47 -0300

    Use Minecraft 26.1 ContainerInput click API

--- src/main/java/mcjty/rftoolsbuilder/constructor/ConstructorMenu.java
+++ src/main/java/mcjty/rftoolsbuilder/constructor/ConstructorMenu.java
@@ -6,7 +6,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.ContainerInput;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
@@ -81,8 +81,8 @@ public final class ConstructorMenu extends AbstractContainerMenu {
     public ContainerData data() { return data; }
 
     @Override
-    public void clicked(int slotId, int button, ClickType clickType, Player player) {
-        if (slotId >= 3 && slotId < 24 && clickType == ClickType.PICKUP && constructor != null) {
+    public void clicked(int slotId, int button, ContainerInput clickType, Player player) {
+        if (slotId >= 3 && slotId < 24 && clickType == ContainerInput.PICKUP && constructor != null) {
             int materialIndex = slotId - 3;
             ItemStack filter = getCarried();
             if (materialIndex < materialSources.size() && filter.getItem() instanceof BlockItem blockItem) {

e97356eff4fe8c8a81a782cfaf72993790c75f31
jeancedrazc-blip <jeancedrazc@gmail.com>
2026-08-21 22:32:51 -0300

    Sync dev.12: MaterialListTabletClient.java

--- src/main/java/mcjty/rftoolsbuilder/constructor/client/MaterialListTabletClient.java
+++ src/main/java/mcjty/rftoolsbuilder/constructor/client/MaterialListTabletClient.java
@@ -2,11 +2,12 @@ package mcjty.rftoolsbuilder.constructor.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.InteractionHand;
 
 public final class MaterialListTabletClient {
     private MaterialListTabletClient() {}
 
-    public static void open(ItemStack tablet) {
-        Minecraft.getInstance().setScreen(new MaterialListTabletScreen(tablet.copy()));
+    public static void open(ItemStack tablet, InteractionHand hand) {
+        Minecraft.getInstance().setScreen(new MaterialListTabletScreen(tablet, hand));
     }
 }
